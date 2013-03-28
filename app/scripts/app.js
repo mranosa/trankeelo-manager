5a209bef49dec423ca7c034403df4bf206f7214b
mranosa <mark.ranosa@gmail.com>
2013-03-28 21:42:51 +0800

    Initial web app items

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var trankeeloManagerApp = angular.module('trankeeloManagerApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
