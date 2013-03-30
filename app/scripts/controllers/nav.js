0201cd9e79f616c0d00514e602ce45804a3f28c1
mranosa <mark.ranosa@gmail.com>
2013-03-31 06:19:34 +0800

    Moved navigation items to controller

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -1,9 +1,39 @@
 'use strict';
 
 trankeeloManagerApp.controller('NavCtrl', function($scope) {
-  $scope.awesomeThings = [
-    'HTML5 Boilerplate',
-    'AngularJS',
-    'Testacular'
+  $scope.navs = [
+  	{
+  		name: 'Dashboard',
+  		iconCls: 'icofont-dashboard',
+  		url: '#/dashboard'
+  	}, {
+  		name: 'Stores',
+  		iconCls: 'icofont-sitemap',
+  		url: '#/stores'
+  	}, {
+  		name: 'Cashier',
+  		iconCls: 'icofont-group',
+  		url: '#/cashier'
+  	}, {
+  		name: 'Template',
+  		iconCls: 'icofont-edit',
+  		url: '#/template'
+  	}, {
+  		name: 'Items',
+  		iconCls: 'icofont-qrcode',
+  		url: '#/items'
+  	}, {
+  		name: 'Inventory',
+  		iconCls: 'icofont-book',
+  		url: '#/inventory'
+  	}, {
+  		name: 'Expenses',
+  		iconCls: 'icofont-money',
+  		url: '#/expenses'
+  	}, {
+  		name: 'History',
+  		iconCls: 'icofont-calendar',
+  		url: '#/history'
+  	}
   ];
 });
