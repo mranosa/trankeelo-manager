a40388d441c18d6a8d3befa2548a8f7fdf967525
mranosa <mark.ranosa@gmail.com>
2013-03-31 06:56:53 +0800

    Added resource module

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var trankeeloManagerApp = angular.module('trankeeloManagerApp', [])
+var trankeeloManagerApp = angular.module('trankeeloManagerApp', ['ngResource'])
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider
       .when('/', {
