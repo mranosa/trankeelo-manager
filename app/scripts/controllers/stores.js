0cec049e6e198d313328627257cb70c3812b555b
mranosa <mark.ranosa@gmail.com>
2013-04-03 02:52:15 +0800

    Added in store CRUD logic

--- app/scripts/controllers/stores.js
+++ app/scripts/controllers/stores.js
@@ -1,5 +1,24 @@
 'use strict';
 
-trankeeloManagerApp.controller('StoresCtrl', ['NavService', '$scope', function(NavService, $scope) {
+trankeeloManagerApp.controller('StoresCtrl', [
+	'NavService', '$scope', 'firebaseCollection', 'firebaseBinding',
+	function(NavService, $scope, firebaseCollection, firebaseBinding) {
   NavService.updateActiveNav();
+
+  $scope.stores = firebaseCollection('https://trankeelo.firebaseio.com/users/test_user/stores');
+  $scope.store = {
+  	name: '',
+  	tin: '',
+  	description: ''
+  };
+
+  $scope.addStore = function(){
+  	$scope.stores.$add($scope.store);
+	$scope.message = {
+		name: '',
+		tin: '',
+		description: ''
+	};
+  }
+
 }]);
