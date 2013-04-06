ff2cc74d3c1fc3bbe0f61987b9d84804f3fa9bc2
mranosa <mark.ranosa@gmail.com>
2013-04-06 23:33:09 +0800

    Fixed add store to datatable

--- app/scripts/controllers/stores.js
+++ app/scripts/controllers/stores.js
@@ -1,33 +1,57 @@
 'use strict';
 
 trankeeloManagerApp.controller('StoresCtrl', [
-	'NavService', '$scope', 'firebaseCollection', 'firebaseBinding',
-	function(NavService, $scope, firebaseCollection, firebaseBinding) {
+	'NavService', '$scope', 'angularFire',
+	function(NavService, $scope, angularFire) {
   NavService.updateActiveNav();
 
-  $scope.stores = firebaseCollection('https://trankeelo.firebaseio.com/users/test_user/stores');
+  var url = 'https://trankeelo.firebaseio.com/users/test_user/stores';
+  $scope.stores = angularFire(url, $scope, 'stores', []);
   $scope.store = {
-  	name: '',
-  	tin: '',
-  	description: ''
+    name: '',
+    tin: '',
+    description: ''
   };
 
-  $scope.addStore = function(){
-  	$scope.stores.$add($scope.store);
-  	$scope.message = {
-  		name: '',
-  		tin: '',
-  		description: ''
-  	};
-  }
+  $scope.stores.then(function(stores) {
+    console.log(stores);
+    $('#datatables').dataTable( {
+        "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
+        "sPaginationType": "bootstrap",
+        "oLanguage": {
+                "sLengthMenu": "_MENU_ records per page"
+        },
+        "aaData" : $scope.stores,
+        "aoColumns": [
+          { "sTitle": "Name",   "mData": "name", "mDataProp" : "name" },
+          { "sTitle": "TIN",  "mData": "tin", "mDataProp" : "tin" },
+          { "sTitle": "Description", "mData": "description", "mDataProp" : "description" }
+        ]
+    });
 
-  // datatables
-  $('#datatables').dataTable( {
-      "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
-      "sPaginationType": "bootstrap",
-      "oLanguage": {
-              "sLengthMenu": "_MENU_ records per page"
-      },
+    $scope.addStore = function(){
+      $scope.stores.push($scope.store);
+      addStoreInDatatable($scope.store);
+      $scope.store = {
+        name: '',
+        tin: '',
+        description: ''
+      };
+    };
+
+    $scope.removeStore = function (store) {
+      $scope.stores.splice($scope.stores.indexOf(store), 1);
+    };
+
+    //TODO implement edit
   });
+
+  function addStoreInDatatable(store){
+  $('#datatables').dataTable().fnAddData({
+    "name" : store.name,
+    "tin" : store.tin,
+    "description" : store.description 
+  });
+  }
 
 }]);
