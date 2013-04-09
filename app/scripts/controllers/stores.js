c789f4b6386765045eeae8c62fae97ecb0fece75
mranosa <mark.ranosa@gmail.com>
2013-04-09 08:15:49 +0800

    Initial ng-grid integration

--- app/scripts/controllers/stores.js
+++ app/scripts/controllers/stores.js
@@ -25,24 +25,26 @@ trankeeloManagerApp.controller('StoresCtrl', [
     $scope.hideList();
     console.log(stores);
 
-    $('#datatables').dataTable( {
-        "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
-        "sPaginationType": "bootstrap",
-        "oLanguage": {
-                "sLengthMenu": "_MENU_ records per page"
-        },
-        "aaData" : $scope.stores,
-        "aoColumns": [
-          { "sTitle": "Name",   "mData": "name", "mDataProp" : "name" },
-          { "sTitle": "TIN",  "mData": "tin", "mDataProp" : "tin" },
-          { "sTitle": "Description", "mData": "description", "mDataProp" : "description" }
-        ]
-    });
+    // $('#datatables').dataTable( {
+    //     "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
+    //     "sPaginationType": "bootstrap",
+    //     "oLanguage": {
+    //             "sLengthMenu": "_MENU_ records per page"
+    //     },
+    //     "aaData" : $scope.stores,
+    //     "aoColumns": [
+    //       { "sTitle": "Name",   "mData": "name", "mDataProp" : "name" },
+    //       { "sTitle": "TIN",  "mData": "tin", "mDataProp" : "tin" },
+    //       { "sTitle": "Description", "mData": "description", "mDataProp" : "description" }
+    //     ]
+    // });
+    console.log($scope.stores);
+
     $scope.page.show_add_button = true;
     $scope.addStore = function(){
       $scope.showList();
       $scope.stores.push($scope.store);
-      addStoreInDatatable($scope.store);
+      // addStoreInDatatable($scope.store);
       $scope.store = {
         name: '',
         tin: '',
@@ -76,14 +78,20 @@ trankeeloManagerApp.controller('StoresCtrl', [
     $scope.page.show_list_table = true;
   };
 
-  function addStoreInDatatable(store){
-    $('#datatables').dataTable().fnAddData({
-      "name" : store.name,
-      "tin" : store.tin,
-      "description" : store.description 
-    });
-  };
+  // function addStoreInDatatable(store){
+  //   $('#datatables').dataTable().fnAddData({
+  //     "name" : store.name,
+  //     "tin" : store.tin,
+  //     "description" : store.description 
+  //   });
+  // };
 
   $scope.showList();
+
+  $scope.storeTableOptions = { 
+    data: 'stores',
+    enableCellEdit: true,
+    enableCellSelection: true
+  };
 
 }]);
